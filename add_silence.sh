b2f963706a783ea54aff75db250bdb0bd047ec39
fbngrm <fbngrm@gmail.com>
2020-02-13 18:07:59 +0100

    remove intermediate track

--- add_silence.sh
+++ add_silence.sh
@@ -5,4 +5,5 @@ for i in *.mp3;
   ffmpeg -i "$i" -filter:a "atempo=0.7" "./slow/${name}_.mp3"
   ffmpeg -i "./slow/${name}_.mp3" -af "apad=pad_dur=1" "./slow/${name}_slow.mp3"
   ffmpeg -i "$i" -af "apad=pad_dur=1" "./slow/$i"
+  rm "./slow/${name}_.mp3"
 done
