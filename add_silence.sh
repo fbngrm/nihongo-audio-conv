005f4698129696ee0625c3470d81c1f31858e3b0
fbngrm <fbngrm@gmail.com>
2020-02-13 18:05:45 +0100

    add original track with silence

--- add_silence.sh
+++ add_silence.sh
@@ -4,5 +4,5 @@ for i in *.mp3;
   echo "$name"
   ffmpeg -i "$i" -filter:a "atempo=0.7" "./slow/${name}_.mp3"
   ffmpeg -i "./slow/${name}_.mp3" -af "apad=pad_dur=1" "./slow/${name}_slow.mp3"
-  rm "./slow/${name}_.mp3"
+  ffmpeg -i "$i" -af "apad=pad_dur=1" "./slow/$i"
 done
