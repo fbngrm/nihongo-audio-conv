d48f12227c5ac7f8b2cd65c9e0115bff646cd173
fbngrm <fbngrm@gmail.com>
2020-02-13 17:53:24 +0100

    fix path

--- add_silence.sh
+++ add_silence.sh
@@ -3,6 +3,6 @@ for i in *.mp3;
   do name=`echo "$i" | cut -d'.' -f1`
   echo "$name"
   ffmpeg -i "$i" -filter:a "atempo=0.7" "./slow/${name}_.mp3"
-  ffmpeg -i "./silent/${name}_.mp3" -af "apad=pad_dur=1" "./slow/${name}_slow.mp3"
+  ffmpeg -i "./slow/${name}_.mp3" -af "apad=pad_dur=1" "./slow/${name}_slow.mp3"
   rm "./slow/${name}_.mp3"
 done
