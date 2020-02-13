929906ae407e5219f64d8fef39a6cbddf3606423
fbngrm <fbngrm@gmail.com>
2020-02-13 17:51:33 +0100

    change dir name

--- add_silence.sh
+++ add_silence.sh
@@ -1,7 +1,8 @@
-mkdir ./silent
+mkdir ./slow
 for i in *.mp3;
   do name=`echo "$i" | cut -d'.' -f1`
   echo "$name"
-  ffmpeg -i "$i" -filter:a "atempo=0.7" "./silent/${name}_.mp3"
-  ffmpeg -i "./silent/${name}_.mp3" -af "apad=pad_dur=1" "./silent/${name}_slow.mp3"
+  ffmpeg -i "$i" -filter:a "atempo=0.7" "./slow/${name}_.mp3"
+  ffmpeg -i "./silent/${name}_.mp3" -af "apad=pad_dur=1" "./slow/${name}_slow.mp3"
+  rm "./slow/${name}_.mp3"
 done
